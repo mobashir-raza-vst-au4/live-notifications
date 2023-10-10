9066d39d6ccd4f231e68c217691cf8e4775c439f
Md Mobashir Raza <mobashir@trimpixel.com>
2023-10-11 00:33:11 +0530

    Implemented get student api

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const cors = require('cors');
 
 // Connect to MongoDB replica set
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.djp5y35.mongodb.net/?retryWrites=true&w=majority`, {
@@ -16,11 +17,20 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
 const enrollmentSchema = new mongoose.Schema({
   studentName: String,
   courseName: String,
+}, {
+  timestamps: true, // This option adds createdAt and updatedAt fields
 });
 
 const Enrollment = mongoose.model('Enrollment', enrollmentSchema);
 
 app.use(express.json());
+
+//CORS
+const corsOption = {
+  origin: ['http://localhost:3000'],
+};
+app.use(cors(corsOption));
+
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -44,6 +54,21 @@ app.post('/enroll', async (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     }
   });
+
+  app.get('/get-students', async (req, res) => {
+    console.log("HITTT GET STUDENTS...")
+    try {
+      //get all data from Enrollement collection in asc order of updatedAt.
+      let enrollments =  await Enrollment.find().sort({'updatedAt': -1}).exec()
+
+      if (!enrollments || !enrollments.length) {
+        throw Error("No students found");
+      }
+      return res.status(200).json(enrollments)
+    } catch (error) {
+      console.log("Error:", error)
+    }
+  })
 
 // Set up a change stream on the enrollment collection
 const enrollmentChangeStream = Enrollment.watch();
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,20 @@ const socket = io('http://localhost:3001', { transports: ['websocket', 'polling'
 
 function App() {
   const [notifications, setNotifications] = useState([]);
+  const [students, setStudents] = useState([]);
+
+  const getAllStudents = async () => {
+    let data = await fetch('http://localhost:3001/get-students')
+    let result = await data.json();
+    setStudents(result)
+
+  }
 
   useEffect(() => {
     socket.on('notification', (message) => {
       setNotifications([...notifications, message]);
     });
+    getAllStudents();
   }, [notifications]);
 
   return (
@@ -19,6 +28,14 @@ function App() {
         {notifications.map((notification, index) => (
           <li key={index}>{notification}</li>
         ))}
+      </ul>
+      <h1>ALL Students</h1>
+      <ul>
+        {students.map((element, index) => {
+          return (
+          <li key={index}>{element._id} - {element.studentName} - {element.courseName}</li>
+          )
+        })}
       </ul>
     </div>
   );
